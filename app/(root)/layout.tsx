f95b39619df676558d3e00152d7aef24169c9955
egidiosalinaro <egy.s96@gmail.com>
2024-05-01 19:24:16 +0200

    lets user upload image

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,16 @@
-import MobileNav from "@/components/shared/MobileNav";
-import Sidebar from "@/components/shared/Sidebar";
+import MobileNav from '@/components/shared/MobileNav';
+import Sidebar from '@/components/shared/Sidebar';
+import { Toaster } from '@/components/ui/toaster';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="root">
+    <main className='root'>
       <Sidebar />
       <MobileNav />
-      <div className="root-container">
-        <div className="wrapper">{children}</div>
+      <div className='root-container'>
+        <div className='wrapper'>{children}</div>
       </div>
+      <Toaster />
     </main>
   );
 };
