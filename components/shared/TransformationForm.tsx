3a99fb7df112b25e936b555169b692b26e057c9a
egidiosalinaro <egy.s96@gmail.com>
2024-05-04 13:59:13 +0200

    update action

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -36,7 +36,7 @@ import { updateCredits } from '@/lib/actions/user.actions';
 import MediaUploader from './MediaUploader';
 import TransformedImage from './TransformedImage';
 import { getCldImageUrl } from 'next-cloudinary';
-import { addImage } from '@/lib/actions/image.actions';
+import { addImage, updateImage } from '@/lib/actions/image.actions';
 import { useRouter } from 'next/navigation';
 
 export const formSchema = z.object({
@@ -101,7 +101,7 @@ const TransformationForm = ({
         width: image?.width,
         height: image?.height,
         config: transformationConfig,
-        secureURL: image?.secureUrl,
+        secureURL: image?.secureURL,
         transformationURL: transformationUrl,
         aspectRatio: values.aspectRatio,
         prompt: values.prompt,
@@ -125,7 +125,25 @@ const TransformationForm = ({
           console.log(error);
         }
       }
+
+      if (action === 'Update') {
+        try {
+          const updatedImage = await updateImage({
+            image: { ...imageData, _id: data._id },
+            userId,
+            path: `/transformations/${data._id}`,
+          });
+
+          if (updatedImage) {
+            router.push(`/transformations/${updatedImage._id}`);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
     }
+
+    setIsSubmitting(false);
   }
 
   const onSelectFieldHandler = (
