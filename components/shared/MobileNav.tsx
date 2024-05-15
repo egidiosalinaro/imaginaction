842faa7d938fb7b7317710d302bd0b3713228575
Egidio Salinaro <egy.s96@gmail.com>
2024-05-15 17:35:34 +0200

    sidebar auto closing #1

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,41 +1,46 @@
-"use client";
+'use client';
 
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { navLinks } from "@/constants";
-import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import Image from "next/image";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { Button } from "../ui/button";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import { navLinks } from '@/constants';
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { Button } from '../ui/button';
 
 const MobileNav = () => {
   const pathname = usePathname();
 
   return (
-    <header className="header">
-      <Link href="/" className="flex items-center gap-2 md:py-2">
-        <Image src="/logo.png" alt="logo" width={180} height={28} />
+    <header className='header'>
+      <Link href='/' className='flex items-center gap-2 md:py-2'>
+        <Image src='/logo.png' alt='logo' width={180} height={28} />
       </Link>
 
-      <nav className="flex gap-2">
+      <nav className='flex gap-2'>
         <SignedIn>
-          <UserButton afterSignOutUrl="/" />
+          <UserButton afterSignOutUrl='/' />
 
           <Sheet>
             <SheetTrigger>
               <Image
-                src="/assets/icons/menu.svg"
-                alt="menu"
+                src='/assets/icons/menu.svg'
+                alt='menu'
                 width={32}
                 height={32}
-                className="cursor-pointer"
+                className='cursor-pointer'
               />
             </SheetTrigger>
-            <SheetContent className="sheet-content sm:w-64">
+            <SheetContent className='sheet-content sm:w-64'>
               <>
-                <Image src="/logo.png" alt="logo" width={152} height={23} />
+                <Image src='/logo.png' alt='logo' width={152} height={23} />
 
-                <ul className="header-nav_elements">
+                <ul className='header-nav_elements'>
                   {navLinks.map((link) => {
                     const isActive = link.route === pathname;
 
@@ -43,21 +48,23 @@ const MobileNav = () => {
                       <li
                         key={link.route}
                         className={`${
-                          isActive && "gradient-text"
+                          isActive && 'gradient-text'
                         } p-18 flex whitespace-nowrap text-dark-700`}
                       >
-                        <Link
-                          className="sidebar-link cursor-pointer"
-                          href={link.route}
-                        >
-                          <Image
-                            src={link.icon}
-                            alt="logo"
-                            width={24}
-                            height={24}
-                          />
-                          {link.label}
-                        </Link>
+                        <SheetClose asChild>
+                          <Link
+                            className='sidebar-link cursor-pointer'
+                            href={link.route}
+                          >
+                            <Image
+                              src={link.icon}
+                              alt='logo'
+                              width={24}
+                              height={24}
+                            />
+                            {link.label}
+                          </Link>
+                        </SheetClose>
                       </li>
                     );
                   })}
@@ -68,8 +75,8 @@ const MobileNav = () => {
         </SignedIn>
 
         <SignedOut>
-          <Button asChild className="button bg-purple-gradient bg-cover">
-            <Link href="/sign-in">Login</Link>
+          <Button asChild className='button bg-purple-gradient bg-cover'>
+            <Link href='/sign-in'>Login</Link>
           </Button>
         </SignedOut>
       </nav>
